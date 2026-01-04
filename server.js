49044a15cb0a0c050f960346d65ef8a84cf3d2e7
Vinny <vincentgordin@gmail.com>
2026-01-03 20:56:21 -0600

    Fix production build path, please work

--- server.js
+++ server.js
@@ -434,10 +434,10 @@ function updateAnnualStats(userId, mediaType) {
 
 // Serve static files in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
   
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
